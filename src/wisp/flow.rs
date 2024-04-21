b42f9ddcfb187f773cb14dd4eed58d7daafb6924
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2024-04-21 15:35:36 +0200

    Implement signal flow compilation

--- src/wisp/flow.rs
+++ src/wisp/flow.rs
@@ -1,13 +1,21 @@
-use std::cell::{Ref, RefCell};
+use std::{
+    cell::{Ref, RefCell},
+    collections::HashMap,
+};
 
-use petgraph::{graph::NodeIndex, stable_graph::StableGraph};
+use petgraph::{
+    graph::NodeIndex,
+    stable_graph::StableGraph,
+    visit::{DfsPostOrder, EdgeRef, Reversed, Walker},
+    Directed, Direction,
+};
 
 use super::{
     ir::{Instruction, VarRef},
     runtime::Runtime,
 };
 
-type FlowGraph = StableGraph<String, FlowConnection>;
+type FlowGraph = StableGraph<String, FlowConnection, Directed>;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub struct FlowFunctionIndex(NodeIndex);
@@ -60,12 +68,53 @@ impl Flow {
         Ref::map(self.ir.borrow(), |is| is.as_ref().unwrap())
     }
 
-    fn compile(&self, _runtime: &Runtime) -> Vec<Instruction> {
-        // TODO: Implement this function
-        // For now, return a hard-coded flow
-        vec![
-            Instruction::Call("test".into(), vec![], vec![VarRef(0)]),
-            Instruction::Call("out".into(), vec![VarRef(0)], vec![]),
-        ]
+    fn compile(&self, runtime: &Runtime) -> Vec<Instruction> {
+        // This function walks the graph from `out` to the nodes leading into it
+        // in post order, so all producing nodes are visited before all consuming
+        // nodes. This allows compiling the signal flow as a series of function calls.
+        let out_idx = self
+            .graph
+            .node_indices()
+            .find(|n| self.graph.node_weight(*n).unwrap() == "out");
+        if out_idx.is_none() {
+            return vec![];
+        }
+        let out_idx = out_idx.unwrap();
+
+        let rev_graph = Reversed(&self.graph);
+        let dfs_post_order = DfsPostOrder::new(&rev_graph, out_idx);
+
+        let mut instructions = vec![];
+
+        let mut vref_id = 0;
+        let mut output_vrefs = HashMap::new();
+        for n in dfs_post_order.iter(&rev_graph) {
+            let func = runtime
+                .get_function(self.graph.node_weight(n).unwrap())
+                .expect("Failed to find function");
+            let mut inputs = vec![];
+            for _ in func.inputs().iter() {
+                for e in self.graph.edges_directed(n, Direction::Incoming) {
+                    let vref = *output_vrefs
+                        .get(&(e.source(), e.weight().output_index))
+                        .expect("Failed to find incoming signal's var ref");
+                    inputs.push(vref);
+                }
+            }
+            let mut outputs = vec![];
+            for (idx, _) in func.outputs().iter().enumerate() {
+                let vref = VarRef(vref_id);
+                outputs.push(vref);
+                vref_id += 1;
+                output_vrefs.insert((n, idx as u32), vref);
+            }
+            instructions.push(Instruction::Call(
+                self.graph.node_weight(n).unwrap().clone(),
+                inputs,
+                outputs,
+            ));
+        }
+
+        instructions
     }
 }
