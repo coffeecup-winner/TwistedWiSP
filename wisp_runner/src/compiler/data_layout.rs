c6658ac2d5481da921a015e0012b6ca6fdf97cfa
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2024-05-29 20:31:34 +0200

    Fix data layout calculation in conditional branches

--- wisp_runner/src/compiler/data_layout.rs
+++ wisp_runner/src/compiler/data_layout.rs
@@ -82,28 +82,14 @@ impl DataLayout {
         called_functions: &mut HashSet<String>,
     ) -> Option<FunctionDataLayout> {
         let mut children_data_sizes = BTreeMap::new();
-        for insn in func.instructions().iter() {
-            match insn {
-                Instruction::Call(id, name, _, _)
-                | Instruction::Load(_, SourceLocation::LastValue(id, name, _)) => {
-                    called_functions.insert(name.into());
-                    if let Some(child_data_layout) = data_layout.get(name) {
-                        children_data_sizes
-                            .insert(*id, (name.into(), child_data_layout.total_size));
-                    } else if let Some(child_data_layout) = Self::calculate_function_data_layout(
-                        wctx.get_function(name).unwrap(),
-                        wctx,
-                        data_layout,
-                        called_functions,
-                    ) {
-                        children_data_sizes
-                            .insert(*id, (name.into(), child_data_layout.total_size));
-                        data_layout.insert(name.into(), child_data_layout);
-                    }
-                }
-                _ => (),
-            }
-        }
+
+        Self::calculate_children_data_sizes(
+            func.instructions(),
+            wctx,
+            data_layout,
+            called_functions,
+            &mut children_data_sizes,
+        );
 
         let mut own_data_offsets = BTreeMap::new();
         let mut total_size = 0;
@@ -133,6 +119,51 @@ impl DataLayout {
             children_data_items: children_data_offsets,
             total_size,
         })
+    }
+
+    fn calculate_children_data_sizes(
+        insns: &[Instruction],
+        wctx: &WispContext,
+        data_layout: &mut HashMap<String, FunctionDataLayout>,
+        called_functions: &mut HashSet<String>,
+        sizes: &mut BTreeMap<CallId, (String, u32)>,
+    ) {
+        for insn in insns {
+            match insn {
+                Instruction::Call(id, name, _, _)
+                | Instruction::Load(_, SourceLocation::LastValue(id, name, _)) => {
+                    called_functions.insert(name.into());
+                    if let Some(child_data_layout) = data_layout.get(name) {
+                        sizes.insert(*id, (name.into(), child_data_layout.total_size));
+                    } else if let Some(child_data_layout) = Self::calculate_function_data_layout(
+                        wctx.get_function(name).unwrap(),
+                        wctx,
+                        data_layout,
+                        called_functions,
+                    ) {
+                        sizes.insert(*id, (name.into(), child_data_layout.total_size));
+                        data_layout.insert(name.into(), child_data_layout);
+                    }
+                }
+                Instruction::Conditional(_, then, else_) => {
+                    Self::calculate_children_data_sizes(
+                        then,
+                        wctx,
+                        data_layout,
+                        called_functions,
+                        sizes,
+                    );
+                    Self::calculate_children_data_sizes(
+                        else_,
+                        wctx,
+                        data_layout,
+                        called_functions,
+                        sizes,
+                    );
+                }
+                _ => (),
+            }
+        }
     }
 
     pub fn get(&self, name: &str) -> Option<&FunctionDataLayout> {
